a6ae0d192b68a6f522bb7c2b61398b0dfb1b7376
quang2002 <62789396+quang2002@users.noreply.github.com>
2021-01-03 15:29:38 +0700

    .

--- JapaneseFoodWeb/settings.py
+++ JapaneseFoodWeb/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +132,5 @@ STATICFILES_URLS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'home', 'static', 'media')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
